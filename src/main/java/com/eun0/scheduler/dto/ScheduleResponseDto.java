ceedc14de149a6b8229e09125f74503ecbadeffa
eun0 <shinely12@gmail.com>
2024-10-04 00:17:23 +0900

    Database의 Naming Rule에 따른 변수명 수정, responseDto 수정, 생성자 추가

--- src/main/java/com/eun0/scheduler/dto/ScheduleResponseDto.java
+++ src/main/java/com/eun0/scheduler/dto/ScheduleResponseDto.java
@@ -1,5 +1,6 @@
 package com.eun0.scheduler.dto;
 
+import com.eun0.scheduler.entity.Schedule;
 import lombok.Getter;
 
 import java.sql.Timestamp;
@@ -9,5 +10,22 @@ public class ScheduleResponseDto {
     private Long id;
     private String writer;
     private String todo;
-    private Timestamp modificationDate;
+    private Timestamp createdDate;
+    private Timestamp updatedDate;
+
+    public ScheduleResponseDto(Schedule schedule) {
+        this.id = schedule.getId();
+        this.writer = schedule.getWriter();
+        this.todo = schedule.getTodo();
+        this.createdDate = schedule.getCreatedDate();
+        this.updatedDate = schedule.getUpdatedDate();
+    }
+
+    public ScheduleResponseDto(Long id, String writer, String todo, Timestamp createdDate, Timestamp updatedDate) {
+        this.id = id;
+        this.writer = writer;
+        this.todo = todo;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+    }
 }
