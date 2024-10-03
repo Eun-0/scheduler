63068ecd3c496219eec8b0a7b570ea8efcedb141
eun0 <shinely12@gmail.com>
2024-10-03 22:35:14 +0900

    '할 일'에 대한 변수명 수정

--- src/main/java/com/eun0/scheduler/entity/Schedule.java
+++ src/main/java/com/eun0/scheduler/entity/Schedule.java
@@ -13,7 +13,7 @@ public class Schedule {
     private Long id;                        // 고유 식별자
     private String writer;                  // 작성자명
     private String password;                // 비밀번호
-    private String toDo;                    // 할 일
+    private String todo;                    // 할 일
     private Timestamp creationDate;         // 최초 작성일
     private Timestamp modificationDate;     // 수정일 (마지막 작성일)
 }
