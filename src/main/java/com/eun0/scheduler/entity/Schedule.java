dbe287bf6a171a447523389306fb921c08edc718
eun0 <shinely12@gmail.com>
2024-10-03 20:39:15 +0900

    Entity 클래스명 변경 (Scheduler -> Schedule), '할 일'에 대한 인스턴스 변수명 변경 (schedule -> toDo)

---
+++ src/main/java/com/eun0/scheduler/entity/Schedule.java
@@ -0,0 +1,19 @@
+package com.eun0.scheduler.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Schedule {
+    private Long id;                        // 고유 식별자
+    private String writer;                  // 작성자명
+    private String password;                // 비밀번호
+    private String toDo;                    // 할 일
+    private Timestamp creationDate;         // 최초 작성일
+    private Timestamp modificationDate;     // 수정일 (마지막 작성일)
+}
